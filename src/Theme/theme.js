82e4913c6a59f2ed3378213cb208ac3d898b689e
DayaneArnaud <dayanearnaud@gmail.com>
2024-04-07 15:57:39 -0300

    [FIX]: corrijindo falhas do tema

--- src/Theme/theme.js
+++ src/Theme/theme.js
@@ -1,18 +1,33 @@
-let currentTheme = "light";
+let currentTheme = localStorage.getItem("theme") || "light";
 
 export const createThemeIcon = () => {
   const themeIcon = document.createElement("i");
-  themeIcon.className = currentTheme === "light" ? "fas fa-sun" : "fas fa-moon";
-  themeIcon.classList.add("theme-icon");
+  themeIcon.classList.add("theme-icon", "fa-solid");
+  if (currentTheme === "light") {
+    themeIcon.classList.add("fa-sun");
+  } else {
+    themeIcon.classList.add("fa-moon");
+  }
+
   themeIcon.addEventListener("click", () => {
-    currentTheme = currentTheme === "light" ? "dark" : "light";
+    if (currentTheme === "light") {
+      themeIcon.classList.replace("fa-sun", "fa-moon");
+      currentTheme = "dark";
+    } else {
+      themeIcon.classList.replace("fa-moon", "fa-sun");
+      currentTheme = "light";
+    }
     toggleTheme(currentTheme);
-    themeIcon.className =
-      currentTheme === "light" ? "fas fa-sun" : "fas fa-moon";
+    localStorage.setItem("theme", currentTheme);
   });
   return themeIcon;
 };
 
 export const toggleTheme = (selectedTheme) => {
   document.documentElement.setAttribute("theme", selectedTheme);
+  localStorage.setItem("theme", selectedTheme);
 };
+
+window.addEventListener("DOMContentLoaded", () => {
+  toggleTheme(currentTheme);
+});
