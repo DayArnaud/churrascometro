20dd05d83510752432994cfc87dbdde2f46817d1
DayaneArnaud <dayanearnaud@gmail.com>
2024-04-10 13:43:21 -0300

    [FEAT]: inserindo lógica e verificações na página de form

--- src/form/form.js
+++ src/form/form.js
@@ -1,4 +1,36 @@
-// import { createThemeIcon } from "../Theme/theme.js";
+import { createThemeIcon } from "../Theme/theme.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nameInput = document.querySelector("input[type='text']");
+    const emailInput = document.querySelector("input[type='email']");
+    const cepInput = document.querySelector("input[type='number']");
+    const checkbox = document.querySelector("#terms");
+
+    if (
+      nameInput.value &&
+      emailInput.value &&
+      cepInput.value &&
+      checkbox.checked
+    ) {
+      const dataForm = {
+        name: nameInput.value,
+        email: emailInput.value,
+        cep: cepInput.value,
+        checkbox: checkbox.checked,
+      };
+      localStorage.setItem("dataForm", JSON.stringify(dataForm));
+    } else {
+      alert(
+        "Por favor, preencha todos os campos e marque o checkbox de termos."
+      );
+    }
+  });
+});
 
 // export const createNewElement = (tag, text = "", attributes = {}) => {
 //   const element = document.createElement(tag);
